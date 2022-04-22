afb5fc2ec94fb5414721dc5d6d3e183f4c00cb23
Sub2Nex <80410032+Sub2Nex@users.noreply.github.com>
2022-04-22 21:07:49 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
  "description": "Ryno is a multi-purpose discord bot ready to skill up and boost up your Discord server!",
  "domain": "pogy.xyz", // domain
  "google_analitics": "", // google analitics
- "token": "NzUxMTQ2MDkwOTQxOTA2OTQ1.X1E1Tw.UKmf_kV6aNaN9j5EE_yNHnO00sY",
+ "token": process.env.TOKEN,
  "https":"https://", // leave as is
  "port":"5003",
 
